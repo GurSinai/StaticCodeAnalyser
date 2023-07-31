35ab60a8be0adafcb2910365970089892919ff25
Oriel <saban.oriel@gmail.com>
2023-07-31 10:50:18 +0300

    realize of Flake8

---
+++ Flake8/Flake8.py
@@ -0,0 +1,30 @@
+import os, subprocess, sys
+
+### Import Interface Module
+parent_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Add the parent directory path to sys.path
+sys.path.append(parent_directory)
+import interface
+ 
+class Flake8Implementation(interface.StaticCodeAnylazer):
+    def scan_file(directory_path, output_path):
+  
+        # Run Flake8 using subprocess
+        command = ['flake8', directory_path, '--output-file', output_path]
+        try:
+            subprocess.run(command, check=True, capture_output=True, text=True)
+            print(f"Flake8 analysis completed successfully. Results saved to: {output_path}")
+            return True
+        except subprocess.CalledProcessError as e:
+            print(f"Error running Flake8: {e.stderr}")
+            return False
+    if __name__ == "__main__":
+        directory_path = "./my_project"
+        output_path = "./flake8_results.txt"
+
+        success = scan_file(directory_path, output_path)
+        if not success:
+            print("Flake8 analysis failed.")
+    
+    
+        
