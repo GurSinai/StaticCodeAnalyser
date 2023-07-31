523f8becfe64e879b8fcc986dc5f235ce8783dfb
Oriel <saban.oriel@gmail.com>
2023-07-31 10:44:32 +0300

    realize of bandit function

---
+++ Bandit/Bandit.py
@@ -0,0 +1,30 @@
+import os, subprocess, sys
+
+### Import Interface Module
+parent_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Add the parent directory path to sys.path
+sys.path.append(parent_directory)
+import interface
+ 
+class BanditImplementation(interface.StaticCodeAnylazer):
+    def scan_file(directory_path, output_path):
+        
+        if not os.path.exists(directory_path):
+            print(f"Directory '{directory_path}' does not exist.")
+            return False
+
+        command = ['bandit', '-r', directory_path, '-o', output_path]
+        try:
+            subprocess.run(command, check=True, capture_output=True, text=True)
+            print(f"Bandit scan completed successfully. Results saved to: {output_path}")
+            return True
+        except subprocess.CalledProcessError as e:
+            print(f"Error running Bandit: {e.stderr}")
+            return False
+    if __name__ == "__main__":
+            directory_path = "./my_project"
+            output_path = "./bandit_results.txt"
+
+            success = scan_file(directory_path, output_path)
+            if not success:
+                print("Bandit scan failed.")
