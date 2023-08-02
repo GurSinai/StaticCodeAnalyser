60176e16b71d27865663c8a22b06575cb0c19bfb
Gur Sinai <gursin384@gmail.com>
2023-08-02 08:17:32 +0300

    Abandoned Flask

--- app.py
+++ app.py
@@ -1,12 +1,1 @@
-from flask import Flask, render_template
-import db
-app = Flask(__name__)
-
-
-
-@app.route('/')
-def index():
-    return render_template('index.html', projects=db.read_all_projects())
-
-if __name__ == '__main__':
-    app.run(debug=True)
+import streamlit as st
