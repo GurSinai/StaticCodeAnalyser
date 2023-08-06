5a9dabd7c5721e1780c9c149bf2e7f6b150ba4f6
Gur Sinai <gursin384@gmail.com>
2023-08-06 19:49:38 +0300

    Fix 4 STA and grouping into analyzers.py

--- LLM-ChatGPT/chat.py
+++ LLM-ChatGPT/chat.py
@@ -5,23 +5,18 @@ parent_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Add the parent directory path to sys.path
 sys.path.append(parent_directory)
 
-
-from Pylint.Pylint import PylintImplementation
+from analyzers import BanditImplementation, Flake8Implementation, PylintImplementation, PyflakesImplementation
+file_ts = './ex1.py'
 PylintI = PylintImplementation()
-#PylintI.scan_file('./db.py', 'LLM-ChatGPT/Pylint/scan.txt')
-
-from Bandit.Bandit import BanditImplementation
 BanditI = BanditImplementation()
-#BanditI.scan_file('./db.py', 'LLM-ChatGPT/Bandit/scan.txt')
-
-from Flake8.Flake8 import Flake8Implementation
 FlakeI = Flake8Implementation()
-#FlakeI.scan_file('./db.py', 'LLM-ChatGPT/Flake8/scan.txt')
-
-from Pyflakes.Pyflakes import PyflakesImplementation
 flakesI = PyflakesImplementation()
-flakesI.scan_file('./db.py', 'LLM-ChatGPT/Pyflakes/scan.txt')
+STA = []
+STA.append(BanditI)
+STA.append(FlakeI)
+STA.append(flakesI)
+STA.append(PylintI)
 
 
-def scan_files(project):
+def scan_file(file_path):
     pass
