b428a77843c1f6de00b33efa6b80a2bc54269fa5
Oriel <saban.oriel@gmail.com>
2023-08-08 21:56:20 +0300

    add def that find which analyzer is call use chat-gpt

--- LLM/chat.py
+++ LLM/chat.py
@@ -1,10 +1,45 @@
-import openai, os, sys
+import openai, os, sys ,fnmatch
 import time
 
 
 parent_directory = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Add the parent directory path to sys.path
 sys.path.append(parent_directory)
+
+def read_file_to_string(file_path):
+    with open(file_path, 'r') as file:
+        file_content = file.read()
+    return file_content
+
+file_path = 'myfile.txt'  # Replace this by text of type of analyzer
+typeofana="bandit"#change this of each type of code (flake8 and more..)
+# Read file content and store it in a string
+sendtochatgpt = "fix this problem code from " + typeofana +" usinig minimum words "  +read_file_to_string(file_path)
+import openai
+
+api_key = "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"
+openai.api_key = api_key
+
+def chat_with_gpt3(prompt):
+    response = openai.Completion.create(
+        engine="gpt-3.5",  # Use GPT-3.5 engine
+        prompt=prompt,
+        max_tokens=400  # Adjust as needed to control the response length
+    )
+    return response['choices'][0]['text']
+
+if __name__ == "__main__":
+    # Prompt to send to GPT-3.5
+    askcode=sendtochatgpt
+    chat_prompt = f"You: {askcode}\nChatGPT: "
+    response = chat_with_gpt3(chat_prompt)
+    #send the responde to website.
+    # Print the response from GPT-3.5
+    print("ChatGPT:", response)
+
+
+
+
 
 from analyzers import BanditImplementation, Flake8Implementation, PylintImplementation, PyflakesImplementation
 file_ts = './ex1.py'
@@ -52,3 +87,34 @@ def scan_project(project_path):
     for file in files:
         if file[-2:] == 'py':
             scan_file(file)
+
+
+
+
+def find_files_with_string(directory, string):
+    matching_files = []
+    typesofA = ['/Pylint/', '/Flake8/', '/Bandit/', '/Pyflakes/']
+    vara = ""
+    for root, _, filenames in os.walk(directory):
+        for filename in filenames:
+            for i in typesofA:
+                if fnmatch.fnmatch(filename, f'*{i}*'):
+                    vara=i
+                    break
+ 
+    if(i=='/Pylint/'):
+     return 'Pylint'
+    if(i=='/Flake8/'):
+     return 'Flake8'  
+    if(i=='/Bandit/'):
+     return 'Bandit'
+    if(i=='/Pyflakes/'):
+     return 'Pyflakes'
+    return "cant find which analyzer use!!"
+directory_to_search = '/path/to/search'
+string_to_find = '/Pylint/'
+
+matching_files = find_files_with_string(directory_to_search, string_to_find)
+for filename in matching_files:
+    print(filename)
+
