37733ad10367a60ce78eea3995f0a6c946618b04
Oriel <saban.oriel@gmail.com>
2023-08-09 09:03:32 +0300

    add array that save path and responed from chat-gpt

--- LLM/chat.py
+++ LLM/chat.py
@@ -11,32 +11,9 @@ def read_file_to_string(file_path):
         file_content = file.read()
     return file_content
 
-file_path = 'myfile.txt'  # Replace this by text of type of analyzer
-typeofana="bandit"#change this of each type of code (flake8 and more..)
-# Read file content and store it in a string
-sendtochatgpt = "fix this problem code from " + typeofana +" usinig minimum words "  +read_file_to_string(file_path)
-import openai
 
 api_key = "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"
 openai.api_key = api_key
-
-def chat_with_gpt3(prompt):
-    response = openai.Completion.create(
-        engine="gpt-3.5",  # Use GPT-3.5 engine
-        prompt=prompt,
-        max_tokens=400  # Adjust as needed to control the response length
-    )
-    return response['choices'][0]['text']
-
-if __name__ == "__main__":
-    # Prompt to send to GPT-3.5
-    askcode=sendtochatgpt
-    chat_prompt = f"You: {askcode}\nChatGPT: "
-    response = chat_with_gpt3(chat_prompt)
-    #send the responde to website.
-    # Print the response from GPT-3.5
-    print("ChatGPT:", response)
-
 
 
 
@@ -91,7 +68,7 @@ def scan_project(project_path):
 
 
 
-def find_files_with_string(directory, string):
+def find_type_analyzer(directory):
     typesofA = ['/Pylint/', '/Flake8/', '/Bandit/', '/Pyflakes/']
     vara = ""
     for root, _, filenames in os.walk(directory):
@@ -102,11 +79,34 @@ def find_files_with_string(directory, string):
                     break
  
     if(vara=='/Pylint/'):
-     return 'Pylint'
+     return "Pylint"
     if(vara=='/Flake8/'):
-     return 'Flake8'  
+     return "Flake8"  
     if(vara=='/Bandit/'):
-     return 'Bandit'
+     return "Bandit"
     if(vara=='/Pyflakes/'):
-     return 'Pyflakes'
+     return "Pyflakes"
     return "cant find which analyzer use!!"
+
+def chat_with_gpt3(prompt):
+    response = openai.Completion.create(
+        engine="gpt-3.5",  # Use GPT-3.5 engine
+        prompt=prompt,
+        max_tokens=400  # Adjust as needed to control the response length
+    )
+    return response['choices'][0]['text']
+
+if __name__ == "__main__":
+    # Prompt to send to GPT-3.5
+    askcode="" 
+    response = []
+    response_file_path=[]
+    for i in OUT_Paths:
+            filess= list_directory_recursive(i)
+            for j in filess:
+                askcode = "fix this problem code from " + find_type_analyzer(i) +" usinig minimum words "  + read_file_to_string(j) ###change read my file!!!
+                chat_prompt = f"You: {askcode}\nChatGPT: " #send the responde to website
+                response.append(chat_with_gpt3(chat_prompt))# save the responde
+                response_file_path.append(j)##Tis save the file path that i save the ans.
+              
+            
